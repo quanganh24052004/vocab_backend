37e9fb6d877623b58dba4e4ad06c0f33d30868a5
Anh Nguyen Quang <anh.nguyenquang24052004@gmail.com>
2026-03-16 00:52:31 +0700

    fix: change base to root and add spa routing for subdomain

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/', // BẮT BUỘC phải là dấu gạch chéo này để chạy subdomain
 })
